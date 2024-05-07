13810bba0b37fc91052dfad1371ceef834d20f97
Kim mangi <aksrl25@mgail.com>
2024-05-07 15:39:05 +0900

    프로잭트 생성 및 swagger 추가

---
+++ src/main/java/com/portfolio/portfolio_was/PortfolioWasApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.portfolio_was;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioWasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioWasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/portfolio_was/PortfolioWasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.portfolio_was;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioWasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
